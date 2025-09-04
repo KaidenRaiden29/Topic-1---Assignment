6b2eb6cef6e1d235e65ac52aef1b4ee4b863f922
362568016 <362568016@2260I-L29-013>
2025-09-04 09:35:38 -0400

    Add project files.

---
+++ Topic 1 - Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Topic_1___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey!");
+            Console.WriteLine();
+            Console.WriteLine("Comments are used becasue you can use them to see where you left off or to read the code easier");
+            Console.WriteLine();
+
+            Console.WriteLine("K"); Console.WriteLine(""); Console.WriteLine(""); Console.WriteLine(""); Console.WriteLine("K");
+            Console.WriteLine("K");
+        }
+    }
+}
