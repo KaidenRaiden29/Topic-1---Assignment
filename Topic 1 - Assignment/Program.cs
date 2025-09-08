6c719773188b5e601cd6647d2a42e891826ab014
Kaiden Reid <362568016@GoTVDSB.ca>
2025-09-08 09:16:56 -0400

    finnished verison

--- Topic 1 - Assignment/Program.cs
+++ Topic 1 - Assignment/Program.cs
@@ -9,14 +9,14 @@
             Console.WriteLine("Comments are used becasue you can use them to see where you left off or to read the code easier");
             Console.WriteLine();
 
-            Console.WriteLine("K"); Console.WriteLine(""); Console.WriteLine(""); Console.WriteLine(""); Console.WriteLine("K");
-            Console.WriteLine("K"); Console.WriteLine(""); Console.WriteLine(""); Console.WriteLine("K");
-            Console.WriteLine("K"); Console.WriteLine(""); Console.WriteLine("K");
-            Console.WriteLine("K"); Console.WriteLine("K");
-            Console.WriteLine("K"); Console.WriteLine(""); Console.WriteLine("K");
-            Console.WriteLine("K"); Console.WriteLine(""); Console.WriteLine(""); Console.WriteLine("K");
-            Console.WriteLine("K"); Console.WriteLine(""); Console.WriteLine(""); Console.WriteLine(""); Console.WriteLine("K");
-
+            Console.WriteLine("K   K RRRR"); //Console.WriteLine(""); Console.WriteLine(""); Console.WriteLine(""); Console.WriteLine("K");  Console.WriteLine("R"); Console.WriteLine("R"); Console.WriteLine("R"); Console.WriteLine("R");
+            Console.WriteLine("K  K  R   R"); //Console.WriteLine(""); Console.WriteLine(""); Console.WriteLine("K");                         Console.WriteLine("R"); Console.WriteLine("R"); Console.WriteLine(""); Console.WriteLine("");  Console.WriteLine("R");
+            Console.WriteLine("K K   R   R"); //Console.WriteLine(""); Console.WriteLine("K");                                                Console.WriteLine("R"); Console.WriteLine(""); Console.WriteLine("");Console.WriteLine(""); Console.WriteLine("R");
+            Console.WriteLine("K     RRRR"); //Console.WriteLine("K");                                                                       Console.WriteLine("R");                                                     Console.WriteLine("R");                     
+            Console.WriteLine("K K   R R"); //Console.WriteLine(""); Console.WriteLine("K");                                                Console.WriteLine("R");                                                     Console.WriteLine("R");
+            Console.WriteLine("K  K  R  R"); //Console.WriteLine(""); Console.WriteLine(""); Console.WriteLine("K");                         Console.WriteLine("R"); Console.WriteLine("R"); Console.WriteLine("R");     Console.WriteLine("R");
+            Console.WriteLine("K   K R   R"); //Console.WriteLine(""); Console.WriteLine(""); Console.WriteLine(""); Console.WriteLine("K");  Console.WriteLine("R"); Console.WriteLine(""); Console.WriteLine("R");
+           
         }
     }
 }
